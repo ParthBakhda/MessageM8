b4cc1d413c6498fce7671375228eb26308a21ce2
Parth Bakhda <bakhdaparth@gmail.com                                                                                                                                                             git config --global user.name Parth>
2023-07-11 21:42:08 -0700

    Added app.js in src folder

--- messenger-app/src/App.js
+++ messenger-app/src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql, useMutation, useQuery } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+});
+
+const GET_MESSAGES = gql`
+  query {
+    messages {
+      id
+      content
+    }
+  }
+`;
+
+const CREATE_MESSAGE = gql`
+  mutation ($content: String!) {
+    createMessage(content: $content) {
+      id
+      content
+    }
+  }
+`;
 
 function App() {
+  const { loading, error, data } = useQuery(GET_MESSAGES);
+  const [createMessage] = useMutation(CREATE_MESSAGE);
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState('');
+
+  useEffect(() => {
+    if (data) {
+      setMessages(data.messages);
+    }
+  }, [data]);
+
+  const handleSendMessage = () => {
+    createMessage({ variables: { content: newMessage } })
+      .then(({ data }) => {
+        setMessages((prevMessages) => [...prevMessages, data.createMessage]);
+        setNewMessage('');
+      })
+      .catch((error) => console.error(error));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Messenger App</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSendMessage();
+        }}
+      >
+        <input
+          type="text"
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+          placeholder="Type your message..."
+        />
+        <button type="submit">Send</button>
+      </form>
+      <ul>
+        {loading ? (
+          <p>Loading messages...</p>
+        ) : (
+          messages.map((message) => (
+            <li key={message.id}>{message.content}</li>
+          ))
+        )}
+      </ul>
     </div>
   );
 }
 
-export default App;
+function AppWrapper() {
+  return (
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  );
+}
+
+export default AppWrapper;
